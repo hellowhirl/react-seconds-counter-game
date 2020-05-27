0f0c161b91375cb7766820a72ce3d6a0f2f58ca8
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-05-27 20:29:57 +0900

    set homepage to '.'

---
+++ docs/precache-manifest.c1ce7ef13c1f0d3283c63f06d1ccf329.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f930b42197afb5e288fdf80d3e070b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee5b150cc37e19050f12",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6fe526d73a3134632c24",
+    "url": "./static/css/main.dc1a8199.chunk.css"
+  },
+  {
+    "revision": "ee5b150cc37e19050f12",
+    "url": "./static/js/2.4d24c870.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4d24c870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe526d73a3134632c24",
+    "url": "./static/js/main.2f6b3402.chunk.js"
+  },
+  {
+    "revision": "99c94cec987f6e9b36a2",
+    "url": "./static/js/runtime-main.1a88532c.js"
+  }
+]);
